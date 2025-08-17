d2bd6b2abcdb6b9054dca0d1417170db3546641e
Prashant Chaudhary <chaudharyprashant7355@gmail.com>
2025-08-17 11:26:07 +0530

    Event Stimulation, Event Delegation and "Focus on Today" mini Project

--- JS/Ch10 (Event Delegation)/script.js
+++ JS/Ch10 (Event Delegation)/script.js
@@ -4,12 +4,12 @@ const container = document.querySelector(".container");
 const addCardButton = document.querySelector(".card");
 const input = document.querySelector("input");
 
-const form = document.querySelector("form");
-const input1 = document.querySelector("#txt-1");
+// const form = document.querySelector("form");
+// const input1 = document.querySelector("#txt-1");
 
-function sayHi() {
-  console.log("Hiii");
-}
+// function sayHi() {
+//   console.log("Hiii");
+// }
 
 function addCard() {
   let card = document.querySelector(".card");
@@ -46,12 +46,28 @@ addCardButton.addEventListener("click", (e) => {
   container.append(newCard);
 });
 
-setTimeout(() => {
-  input.focus();
-  console.log("Input Focused...");
-}, 2000);
+// setTimeout(() => {a
+//   input.focus();
+//   console.log("Input Focused...");
+// }, 2000);
 
-setTimeout(() => {
-  input.blur();
-  console.log("Input Blurred...");
-}, 4000);
+// setTimeout(() => {
+//   input.blur();
+//   console.log("Input Blurred...");
+// }, 4000);
+
+//  Below code is for Event Delegation:
+
+const cardsList = document.querySelectorAll(".card");
+cardsList.forEach((card) => {
+  console.log(card);
+  card.addEventListener("click", () => {
+    card.remove();
+  });
+});
+
+container.addEventListener("click", (e) => {
+  if (e.target !== container) {
+    e.target.remove();
+  }
+});
