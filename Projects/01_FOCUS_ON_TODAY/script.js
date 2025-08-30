0e95180198cc7f1bd1cebb852e3a7109a911df03
Prashant Chaudhary <chaudharyprashant7355@gmail.com>
2025-08-30 10:13:22 +0530

    Local Storage & Mini Project

--- Projects/01_FOCUS_ON_TODAY/script.js
+++ Projects/01_FOCUS_ON_TODAY/script.js
@@ -2,16 +2,20 @@ const checkBoxList = document.querySelectorAll(".custom-checkbox");
 const inputFields = document.querySelectorAll(".goal-input");
 const progressBar = document.querySelector(".progress-bar");
 const progressValue = document.querySelector(".progress-value");
+const allGoals = JSON.parse(localStorage.getItem("allGoals")) || {};
 
 checkBoxList.forEach((checkbox) => {
   checkbox.addEventListener("click", (e) => {
     const allGoalsAdded = [...inputFields].every((input) => {
       return input.value;
     });
-    console.log(allGoalsAdded);
+    // console.log(allGoalsAdded);
+
     if (allGoalsAdded) {
+
       checkbox.parentElement.classList.toggle("completed");
       progressValue.style.width = "33.33%";
+      console.log(allGoals);
     } else {
       progressBar.classList.add("show-error");
     }
@@ -19,7 +23,20 @@ checkBoxList.forEach((checkbox) => {
 });
 
 inputFields.forEach((input) => {
+  input.value = allGoals[input.id].name;
+
+  if (allGoals[input.id].completed) {
+    input.parentElement.classList.add("completed");
+  }
   input.addEventListener("focus", () => {
     progressBar.classList.remove("show-error");
+  });
+
+  input.addEventListener("input", (e) => {
+    allGoals[input.id] = {
+      name: input.value,
+      completed: false,
+    };
+    localStorage.setItem("allGoals", JSON.stringify(allGoals));
   });
 });
