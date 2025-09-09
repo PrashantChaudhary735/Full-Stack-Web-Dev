f74a2cb3ae9f1dc6e006cb1180f849e8c6aaf95a
Prashant Chaudhary <chaudharyprashant7355@gmail.com>
2025-09-09 21:58:54 +0530

    Callback hell

--- JS/Ch13_CallBack Hell/script.js
+++ JS/Ch13_CallBack Hell/script.js
@@ -9,13 +9,27 @@ function makeHttpRequest(method, url, callback) {
   xhr.send();
 }
 
-makeHttpRequest("GET", "https://dummyjson.com/users", (data) => {
+makeHttpRequest("GET", "https://dummyjson.com/users", (usersData) => {
   // console.log(data);
   makeHttpRequest(
     "GET",
-    `https://dummyjson.com/posts/user/${data.users[0].id}`,
-    (data) => {
-      console.log(data.posts[0]);
+    `https://dummyjson.com/posts/user/${usersData.users[0].id}`,
+    (postsData) => {
+      console.log(postsData.posts[0].id);
+      makeHttpRequest(
+        "GET",
+        `https://dummyjson.com/comments/post/${postsData.posts[0].id}`,
+        (commentsData) => {
+          console.log(commentsData.comments[0].user.id);
+          makeHttpRequest(
+            "GET",
+            `https://dummyjson.com/user/${commentsData.comments[0].user.id}`,
+            (usersData2) => {
+              console.log(usersData2);
+            }
+          );
+        }
+      );
     }
   );
 });
