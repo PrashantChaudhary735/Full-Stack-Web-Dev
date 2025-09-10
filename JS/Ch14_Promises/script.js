5fb790d1ac5b6a5d25a5d07d772e8281e8c295a8
Prashant Chaudhary <chaudharyprashant7355@gmail.com>
2025-09-10 21:03:04 +0530

    Promises in JS

---
+++ JS/Ch14_Promises/script.js
@@ -0,0 +1,25 @@
+const resolveBtn = document.querySelector("#resolve-btn");
+const rejectBtn = document.querySelector("#reject-btn");
+
+// const p = new Promise((resolve, reject) => {
+//   resolveBtn.addEventListener("click", () => {
+//     resolve("Promise Resolved");
+//   });
+//   rejectBtn.addEventListener("click", () => {
+//     reject("Promise Rejected");
+//   });
+// });
+
+// p.then((data) => {
+//   console.log(data);
+// })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.log(" Finally");
+//   });
+
+// setTimeout(() => {
+//   console.log("Hi");
+// }, 2000);
