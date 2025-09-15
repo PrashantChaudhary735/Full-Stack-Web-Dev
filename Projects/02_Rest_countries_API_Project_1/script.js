f93874f94e77f4428cd29da04e4e09bfcb158595
Prashant Chaudhary <chaudharyprashant7355@gmail.com>
2025-09-15 22:10:08 +0530

    Rest_Countrie_API project using JS

---
+++ Projects/02_Rest_countries_API_Project_1/script.js
@@ -0,0 +1,24 @@
+const countriesContainer = document.querySelector(".countries-container");
+
+fetch("https://restcountries.com/v3.1/independent?status=true")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((country) => {
+      // console.log(country);
+      const countryCard = document.createElement("a");
+      countryCard.href = `country.html?name=${country.name.common}`;
+      countryCard.classList.add("country-card");
+      countryCard.innerHTML = `
+<img src="${country.flags.svg}" alt="flag" />
+          <div class="card-text">
+            <h3 class="card-title"> ${country.name.common}</h3>
+            <p><b>Population:</b>${country.population.toLocaleString(
+              "en-IN"
+            )}</p>
+            <p><b>Region: </b>${country.region}</p>
+            <p><b>Capital: </b>${country.capital}</p>
+          </div>`;
+      console.log(countriesContainer);
+      // countriesContainer.append(countryCard);
+    });
+  });
